50a503270d61c47e1044313f0564d5405f5223bc
FearDread <ghaptonstall@gmail.com>
2016-05-05 10:45:00 -0700

    --add needed pages directory and gui app js file with base module syntax

---
+++ js/app.js
@@ -0,0 +1,27 @@
+/* Guerilla UI Module */
+/* ================== */
+$.GUI().create('1stchoice', function (gui) {
+
+    function init() {
+
+    }
+
+    function init_plugins() {
+
+    }
+
+    return {
+
+        load: function () {
+
+	    init();
+
+	},
+
+	unload: function () {
+
+	}
+
+    }
+
+}).start();
