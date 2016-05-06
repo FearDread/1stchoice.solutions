76036efd70bc40b650cc1cf1668cadb75217ca75
FearDread <ghaptonstall@gmail.com>
2016-05-06 12:29:51 -0700

    --update main guerrilla app module logic and start adding needed methods.

--- js/guerilla/app.js
+++ js/guerilla/app.js
@@ -1,20 +1,34 @@
 /* Guerilla UI Module */
 /* ================== */
-$.GUI().create('1stchoice', function (gui) {
+$.GUI().create('1stchoice', function ($gui) {
 
-    function init() {
+    function init_plugins() {
 
     }
 
-    function init_plugins() {
+    function init_effects() {
 
     }
 
     return {
 
         load: function () {
+	    $gui.log('loading 1stchoice app');
+	    console.log('loading 1stchoice app ... console');
 
-	    init();
+	    Royal_Preloader.config({
+	        mode: 'number',
+		showProgress: false,
+		background: '#CCCCCC'
+	    });
+
+	    $gui.timeout(function () {
+
+		init_plugins();
+
+		init_effects();
+
+ 	    }, 500);
 
 	},
 
