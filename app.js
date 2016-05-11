2fdd0bf5e5ea92f06241d516bd446f998e9b46b9
FearDread <ghaptonstall@gmail.com>
2016-05-10 17:14:35 -0700

    --update main node app.js script and add favicon to public dir

--- app.js
+++ app.js
@@ -1,66 +1,79 @@
+/* 1st Choice Business Solutions *
+ *                               *
+ * Main Application Script       *
+ * ----------------------------- */
+var app, routes, users;
 
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+routes = require('./routes/index');
+users = require('./routes/users');
 
-var app = express();
+app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Make our db accessible to our router
-app.use(function(req,res,next){
-req.db = db;
-next();
+app.use(function (req, res, next) {
+
+    req.db = db;
+
+    next();
+
 });
 
 app.use('/', routes);
-app.use('/users', users);
+// app.use('/users', users);
 
 /// catch 404 and forwarding to error handler
-app.use(function(req, res, next) {
-var err = new Error('Not Found');
-err.status = 404;
-next(err);
-});
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
 
-/// error handlers
+    err.status = 404;
+
+    next(err);
+
+});
 
 // development error handler
-// will print stacktrace
 if (app.get('env') === 'development') {
-app.use(function(err, req, res, next) {
-res.status(err.status || 500);
-res.render('error', {
-message: err.message,
-error: err
-});
-});
+
+    app.use(function (err, req, res, next) {
+
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+
+    });
+
 }
 
 // production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-res.status(err.status || 500);
-res.render('error', {
-message: err.message,
-error: {}
-});
+app.use(function (err, req, res, next) {
+
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+
 });
 
 module.exports = app;
