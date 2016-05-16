3a0cf854af75552845a059dd18f7eb10711f715a
Garrett Haptonstall <ghaptonstall@gmail.com>
2016-05-16 16:43:32 -0700

    --add needed pages, faq, contact, order and packages with updated markup

--- routes/index.js
+++ routes/index.js
@@ -15,10 +15,40 @@ exports.add = function (app) {
 
     });
 
+    app.get('/about', function (req, res) {
+   
+        res.render('about', {});
+
+    });
+
     app.get('/packages', function (req, res) {
 
         res.render('packages', {});
 
+    });
+
+    app.get('/order', function (req, res) {
+
+        res.render('order', {});
+
+    });
+
+    app.get('/faq', function (req, res) {
+  
+        res.render('faq', {});
+
+    });
+
+    app.get('/contact', function (req, res) {
+
+        res.render('contact', {});
+
+    });
+
+    app.post('/contact', function (req, res) {
+        console.log('posting to /contact');
+
+        res.send('200');
     });
 
 };
